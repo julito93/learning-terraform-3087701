56bc16da116d3510380ebb9b7876016fc0e7b007
vn542da <julio.campaz@walmart.com>
2023-11-30 22:05:33 -0500

    Add load balancer

--- main.tf
+++ main.tf
@@ -37,25 +37,54 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = [aws_instance.blog.id]
+
+    }
+  ]
+  listeners = {
+    http_tcp_listeners = {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog"
-
-  vpc_id  = module.blog_vpc.vpc_id
   
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  name                = "blog"
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules =  ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        =  ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
